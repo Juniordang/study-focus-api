5575df556c47dae7fc246a7736465169bf55dc6d
juniordev <juniordange4@gmail.com>
2026-04-18 01:20:50 -0300

    fix: remove bloco que cria pasta db

--- internal/data/conf/sqlite.go
+++ internal/data/conf/sqlite.go
@@ -1,8 +1,6 @@
 package conf
 
 import (
-	"os"
-
 	"github.com/Juniordang/study-focus-api/cmd/config"
 	"github.com/Juniordang/study-focus-api/internal/data/schema"
 	"github.com/glebarez/sqlite"
@@ -13,23 +11,6 @@ func IntializeSQLite() (*gorm.DB, error) {
 	logger := config.GetLogger("sqlite")
 
 	dbConf := Load()
-
-	_, err := os.Stat(dbConf.DBPath)
-	if os.IsNotExist(err) {
-		logger.Info("database file not found, creating...")
-
-		err := os.MkdirAll("./db", os.ModePerm)
-		if err != nil {
-			return nil, err
-		}
-
-		file, err := os.Create(dbConf.DBPath)
-		if err != nil {
-			return nil, err
-		}
-
-		file.Close()
-	}
 
 	db, err := gorm.Open(sqlite.Open(dbConf.DNS),
 		&gorm.Config{})
