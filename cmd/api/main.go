630d506a815da0d9b48f22b609848c8802e4f885
juniordev <juniordange4@gmail.com>
2026-04-18 01:22:08 -0300

    feat: inicia logger e sqlite

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"github.com/Juniordang/study-focus-api/cmd/config"
+	"github.com/Juniordang/study-focus-api/internal/data/conf"
+)
+
+var logger *config.Logger
 
 func main() {
-	fmt.Println("teste")
+	logger = config.Newlogger("main")
 
+	err := conf.Init()
+	if err != nil {
+		logger.Errf("config initialization error: %v", err)
+		return
+	}
 }
